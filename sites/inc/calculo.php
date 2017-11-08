e075b6b0050a32667281338c4ab8f24fad1fbc32
Rafael De Lima Hernandez <rafael.hernandez@sf.intra>
2017-11-08 10:03:24 -0200

    Intro curso-php

---
+++ sites/inc/calculo.php
@@ -0,0 +1,11 @@
+<?php
+
+class Calculo{
+
+    public function soma($valorA, $valorB){
+        return $valorA + $valorB;
+    }
+
+}
+
+?>
---
+++ sites/index.php
@@ -0,0 +1,17 @@
+<?php
+    
+    require_once("inc/frete.php");
+
+    $frete = new Frete(
+        $cepDeOrigem = '14095050',
+        $cepDeDestino = '14390000',
+        $peso = 0.5,
+        $comprimento = 4,
+        $altura = 12,
+        $largura = 16,
+        $valor = 18
+    );
+
+    echo("Frete: R$ ".$frete->getValor());
+
+?>
